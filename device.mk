31f70cb8300743eef3ee57c9ca2b24d7d9130db1
Anouarbery <sirinaberriri@gmail.com>
2025-08-11 10:42:50 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := device/samsung/a55x
+LOCAL_PATH := device/samsung/a56x
 
 # Enable virtual A/B OTA
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
